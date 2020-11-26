55ccbf2972c93ab320c264ea3e62c5b65232a65d
BriyidCoy <dayiscoy19@gmail.com>
2020-11-26 17:52:55 -0500

    Mejoras gama cromática y contacto con el autor

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+    $Destino = "dayiscoy19@gmail.com";
+    $Nombre = $_POST["Nombre"]; 
+    $Correo_electrónico = $_POST["Correo electrónico"];
+    $Teléfono = $_POST["Teléfono"];
+    $Mensaje = $_POST["Mensaje"];
+    $Contenido = "Nombre:". $Nombre. "\nCorreo:". $Correo_electrónico. "\nTeléfono:". $Teléfono. "\nMensaje". $Mensaje;
+    mail($Destino, "Contacto git", $Contenido);
+    header("Gracias");
+?>
